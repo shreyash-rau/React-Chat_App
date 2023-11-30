e0b04a08eddb6c77e6f7d6b2d1abe7cbb3d1b7f2
Shreyash_Raut <67021340+shreyash-rau@users.noreply.github.com>
2023-11-30 13:05:18 +0530

    Update Input.jsx

--- src/Component/Input.jsx
+++ src/Component/Input.jsx
@@ -1,27 +1,6 @@
-// import React from 'react'
 
 import Attach from "../image/attach.png"
 import Img from "../image/img.png"
-
-// const Input = () => {
-//   return (
-//     <div className="input">
-//       <input type="text" placeholder='Type Your Message...' />
-      
-//       <div className="send">
-//         <img src={Attach} alt="" />
-//         <input type="file" style={{display:"none"}} id="file" />
-//         <label htmlFor="file">
-//           <img src={Img} alt="" />
-//         </label>
-//         <button>Send</button>
-
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default Input
 
 import React, { useContext, useState } from "react";
 import { AuthContext } from "../context/AuthContext";
