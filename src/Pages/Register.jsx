e7c82d337471e891d0938dcae9e1297474944db0
Shreyash_Raut <67021340+shreyash-rau@users.noreply.github.com>
2023-11-30 13:04:09 +0530

    Update Register.jsx

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -12,7 +12,6 @@ import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 import { setDoc, doc } from 'firebase/firestore';
 
 
-
 const Register = () => {
 
     const [err, setErr] = useState(false);
